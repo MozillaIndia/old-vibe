cf70c24f8fce9ec3a7aa8b33318b3e468985bfc9
sudheesh001 <sudheesh1995@outlook.com>
2014-04-06 18:59:12 +0530

    Month Fix [Now shows in word]

--- assets/js/fetchData.js
+++ assets/js/fetchData.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$.get('./data', function(data){
 		if (data){
 			var string = "";
+			var months = new Array("","JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC");
 			var events = data.objects;
 			for (var i=events.length-1;i>=0;i--) {
 				var event = events[i];
@@ -9,7 +10,7 @@ $(document).ready(function(){
 				string+= "<section class=\"event\"><p class=\"calendar event-time\"><time datetime=\"";
 				string+= new Date(event.start);
 				string+= "\">";
-				string+= "<span class=\"calendar-month\">"+date.getMonth()+"</span>";
+				string+= "<span class=\"calendar-month\">"+months[date.getMonth()]+"</span>";
 				string+= "<span class=\"calendar-date\">"+date.getDate()+"</span>";
 				string+= "<span class=\"calendar-year\">"+date.getYear()+"</span>";
 				string+= "</time></p>";
@@ -17,7 +18,7 @@ $(document).ready(function(){
 				string+= "<p>"+event.description+"</p>";
 				string+= "</section>";
 			}
-			debugger
+			//debugger
 			$(".span8").html(string);
 		}
 	})
