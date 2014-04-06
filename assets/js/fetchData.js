c4d5c14a55b77082cef88e446e81a998061e61ba
sudheesh001 <sudheesh1995@outlook.com>
2014-04-06 18:31:29 +0530

    Presentable Hack

--- assets/js/fetchData.js
+++ assets/js/fetchData.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 	$.get('./data', function(data){
 		if (data){
 			var string = "";
-			events = data.objects;
-			for (var i=0;i<events.length;i++) {
-				event = events[i];
+			var events = data.objects;
+			for (var i=events.length-1;i>=0;i--) {
+				var event = events[i];
 				date = new Date(event.start);
 				string+= "<section class=\"event\"><p class=\"calendar event-time\"><time datetime=\"";
 				string+= new Date(event.start);
