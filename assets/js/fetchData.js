ef22f3c108b4a0e1accbe25a135924c769ee5fd4
trishul <trishul.goel@gmail.com>
2015-05-10 13:06:53 +0530

    Issue #34  Put up a search box for city based event search

--- assets/js/fetchData.js
+++ assets/js/fetchData.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+
+
+	$('#searchMe').keyup(function(){
+		populateData($(this).val());
+	});
+	populateData();
+});
+
+function populateData(city){
 	$.get('./data', function(data){
 		if (data){
 			var string = "";
@@ -6,20 +15,22 @@ $(document).ready(function(){
 			var events = data.objects;
 			for (var i=0;i<events.length;i++) {
 				var event = events[i];
-				date = new Date(event.start);
-				string+= "<section class=\"event\"><p class=\"calendar event-time\"><time datetime=\"";
-				string+= new Date(event.start);
-				string+= "\">";
-				string+= "<span class=\"calendar-month\">"+months[date.getMonth()]+"</span>";
-				string+= "<span class=\"calendar-date\">"+date.getDate()+"</span>";
-				string+= "<span class=\"calendar-year\">"+date.getFullYear()+"</span>";
-				string+= "</time></p>";
-				string+= "<h3><a href=\""+event.event_url+"\">"+event.name+"</a></h3>";
-				string+= "<p>"+event.description+"</p>";
-				string+= "</section>";
+				if(city=="" || typeof city=="undefined" || event.city.toLowerCase().indexOf(city)>=0) {	
+					date = new Date(event.start);
+					string+= "<section class=\"event\"><p class=\"calendar event-time\"><time datetime=\"";
+					string+= new Date(event.start);
+					string+= "\">";
+					string+= "<span class=\"calendar-month\">"+months[date.getMonth()]+"</span>";
+					string+= "<span class=\"calendar-date\">"+date.getDate()+"</span>";
+					string+= "<span class=\"calendar-year\">"+date.getFullYear()+"</span>";
+					string+= "</time></p>";
+					string+= "<h3><a href=\""+event.event_url+"\">"+event.name+"</a></h3>";
+					string+= "<p>"+event.description+"</p>";
+					string+= "</section>";
+				}
 			}
 
 			$("#centfix").html(string);
 		}
-	})
-});
+	});
+}
